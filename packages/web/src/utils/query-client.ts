24ca484c129fb8302ecf13d08bbc88e670bbeea2
Matthew Warman <warmanmr@gmail.com>
2026-05-12 08:55:35 -0400

    chore: remove gcTime and retry options from QueryClient configuration

--- packages/web/src/utils/query-client.ts
+++ packages/web/src/utils/query-client.ts
@@ -8,8 +8,6 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 1000 * 60 * 5, // 5 minutes
-      gcTime: 1000 * 60 * 10, // 10 minutes (formerly cacheTime)
-      retry: 1,
     },
   },
 });
